60d77aca0702c322b393f827f29a3b29fbd77f95
Vũ Văn Đạt <58060750+Okami00@users.noreply.github.com>
2021-06-04 13:41:57 +0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var modal = document.getElementById('myModal');
+var btnOption = document.querySelector('.btn--option');
+
+function showDialog(){
+    modal.style.display = 'inline';
+}
+function exit(){
+    modal.style.display = 'none';
+}
