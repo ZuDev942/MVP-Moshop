9058aac6a91eb4c0007b528ec50e91c4b314a4d8
Vũ Văn Đạt <58060750+Okami00@users.noreply.github.com>
2021-06-07 23:07:28 +0700

    Update Popup

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 var modal = document.getElementById('myModal');
 var btnOption = document.querySelector('.btn--option');
+let startDate = document.getElementById('dateStart');
+let endDate = document.getElementById('dateEnd');
+let hasChanged = false;
+let accept;
 
-function showDialog(){
+startDate.onchange = () => {
+    hasChanged = true;
+}
+endDate.onchange = () => {
+    if (hasChanged) {
+        accept = document.querySelector('.modal__btn--accept');
+        accept.style.backgroundColor = 'var(--primary-color)'
+        accept.disabled = false;
+        accept.addEventListener('click', () => {
+            modal.style.display = 'none';
+        })
+    }
+}
+function showDialog() {
     modal.style.display = 'inline';
 }
-function exit(){
+function exit() {
+    clear()
     modal.style.display = 'none';
+}
+function clear() {
+    accept.disabled = true;
+    accept.style.backgroundColor = 'var(--blur)'
+    startDate.value = ""
+    endDate.value = ""
 }
